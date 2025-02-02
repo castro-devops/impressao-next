33dc6d0dcbe9bd97cb048d052ec258031a26ed4f
RASCUNHO papel e design <rascunho.papeledesign@gmail.com>
2025-02-01 21:39:36 -0300

    ajuste de trace e build prisma

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  productionBrowserSourceMaps: false, // Desabilita o trace
+};
 
 export default nextConfig;
